23b58513e6f9cd2dae1f35f5f2cc19b07578d0d8
Jing Lin <moderatelyfunctionalprogrammer@gmail.com>
2019-07-20 16:01:26 -0700

    should be ignored

--- requests.js
+++ requests.js
@@ -7,9 +7,9 @@ const MINS_PER_HOUR = 60;
 const HOURS_PER_DAY = 24;
 
 const DUMMY_ENDPOINT = 'http://dummy.restapiexample.com/api/v1/employee';
-const HERE_ENDPOINT = '';
+const HERE_ENDPOINT = 'https://route.api.here.com/routing/7.2/calculateroute.json';
 
-const submitQuery = function(callback) {
+const submitQuery = function() {
 	const today = new Date();
 	const curr_hour = today.getHours();
 	const curr_minute = today.getMinutes();
@@ -18,11 +18,14 @@ const submitQuery = function(callback) {
 	const query_deferred = [];
 	const query_data = [];
 
-
+	console.log(APP_ID);
 	const tot_minutes = MINS_PER_HOUR * HOURS_PER_DAY;
 	for (let i = curr_time; i < tot_minutes; i+=QUERY_FREQ_IN_MIN) {
 		const deferred = $.ajax({
 			url: `${DUMMY_ENDPOINT}/${i}`,
+			data: {
+				// app_id: 
+			},
 			success: function(result) {
 				console.log("HI");
 				query_data.push([i, result])
@@ -30,7 +33,24 @@ const submitQuery = function(callback) {
 
 		});
 		query_deferred.push(deferred)
+		break
 	}
+
+	// for (let i = curr_time; i < tot_minutes; i+=QUERY_FREQ_IN_MIN) {
+	// 	const deferred = $.ajax({
+	// 		url: `${DUMMY_ENDPOINT}/${i}`,
+	// 		data: {
+
+	// 		}
+	// 		success: function(result) {
+	// 			console.log("HI");
+	// 			query_data.push([i, result])
+	// 		}
+
+	// 	});
+	// 	query_deferred.push(deferred)
+	// 	break
+	// }
 
 	$.when.apply($, query_deferred).then(function() {
 		query_data.sort(function(a, b) {
