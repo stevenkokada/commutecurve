7f42f78c884989d2776ac00a7de2ccaf66b7f934
Jing Lin <moderatelyfunctionalprogrammer@gmail.com>
2019-07-20 14:57:39 -0700

    async material

--- requests.js
+++ requests.js
@@ -9,32 +9,34 @@ const HOURS_PER_DAY = 24;
 const DUMMY_ENDPOINT = 'http://dummy.restapiexample.com/api/v1/employee';
 const HERE_ENDPOINT = '';
 
-const submitQuery = function() {
+const submitQuery = function(callback) {
 	const today = new Date();
 	const curr_hour = today.getHours();
 	const curr_minute = today.getMinutes();
 	const curr_time = curr_hour * MINS_PER_HOUR + curr_minute;
 
-	const query_times = [];
+	const query_deferred = [];
 	const query_data = [];
+
+
 	const tot_minutes = MINS_PER_HOUR * HOURS_PER_DAY;
 	for (let i = curr_time; i < tot_minutes; i+=QUERY_FREQ_IN_MIN) {
-		query_times.push(i);
+		const deferred = $.ajax({
+			url: `${DUMMY_ENDPOINT}/${i}`,
+			success: function(result) {
+				console.log("HI");
+				query_data.push([i, result])
+			}
+
+		});
+		query_deferred.push(deferred)
 	}
 
-	console.log(query_times)
-	$.when(
-		query_times.forEach(query_time => {
-			$.get(`${DUMMY_ENDPOINT}/${query_time}`, function(element) {
-				query_data.push([query_time, element]);
-			})
-		})
-
-	).then(function() {
+	$.when.apply($, query_deferred).then(function() {
 		query_data.sort(function(a, b) {
-			return a[0] - b[0];
+			return a[0] - b[0]
 		});
-		console.log(query_data);
-	})
+		console.log(query_data)
+	});
 }
 
